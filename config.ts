aa1c0c956b83b1ceb5725c6352e97fb06618113c
Anirban Biswas <139000437+ANIR1604@users.noreply.github.com>
2024-12-08 05:12:26 +0530

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
-export const key = "dbf8dbea6d22d11b8d63bef7f571e18f6170f78452a08df2";
+export const key = "your_key";
 
-export const secret = "8b1e091f72529cf4e709e1bcc7729a2875ddee01d06485849f5c8c6a53b62517";
+export const secret = "your_secret";
